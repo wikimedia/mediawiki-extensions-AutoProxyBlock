7b5b304b0cd14bb27f745451e65cc4c677321cb5
Siebrand Mazeland <siebrand@kitano.nl>
2014-03-30 23:23:11 +0200

    Migrate to JSON i18n
    Procedure per https://www.mediawiki.org/wiki/Manual:GenerateJsonI18n.php
    with shim.
    
    Change-Id: I58a7f5e6c160b1632f48d44fc7891316bff61009

--- AutoProxyBlock.php
+++ AutoProxyBlock.php
@@ -8,37 +8,38 @@ $wgExtensionCredits['antispam'][] = array(
 	'path'           => __FILE__,
 	'name'           => 'AutoProxyBlock',
 	'author'         => 'Cryptocoryne',
-	'version'        => '1.0',
+	'version'        => '1.1.0',
 	'descriptionmsg' => 'autoproxyblock-desc',
 	'url'            => 'https://www.mediawiki.org/wiki/Extension:AutoProxyBlock',
 );
- 
+
+$wgMessagesDirs['AutoProxyBlock'] = __DIR__ . '/i18n';
 $wgExtensionMessagesFiles['AutoProxyBlock'] =  "$dir/AutoProxyBlock.i18n.php";
 $wgAutoloadClasses['AutoProxyBlock'] = "$dir/AutoProxyBlock.body.php";
- 
+
 // protect special accounts from tagging proxy edits
 $wgAvailableRights[] = 'notagproxychanges';
- 
+
 // set hooks
 $wgHooks['getUserPermissionsErrorsExpensive'][] = 'AutoProxyBlock::checkProxy';
 $wgHooks['RecentChange_save'][] = 'AutoProxyBlock::tagProxyChange';
 $wgHooks['ListDefinedTags'][] = 'AutoProxyBlock::addProxyTag';
 $wgHooks['AbuseFilter-filterAction'][] = 'AutoProxyBlock::AFSetVar';
 $wgHooks['AbuseFilter-builder'][] = 'AutoProxyBlock::AFBuilderVars';
- 
+
 // array of actions, allowed to perform by proxy users
 $wgProxyCanPerform = array( 'read', 'edit', 'upload' );
- 
+
 // add "proxy" tag to all edits, matched by this extension
 $wgTagProxyActions = true;
- 
+
 // api is mediawiki api url, raw is local path to file with raw proxylist
 // key is regexp to check reason of block, retrieved from api
 $wgAutoProxyBlockSources = array();
 $wgAutoProxyBlockSources['api'][] = 'http://en.wikipedia.org/w/api.php';
 $wgAutoProxyBlockSources['raw'][] = '/var/www/mediawiki/proxy.list';
 $wgAutoProxyBlockSources['key'] = '/blocked proxy/i';
- 
+
 // if set to true, log all blocked actions in Special:Log/proxyblock
 $wgAutoProxyBlockLog = false;
 
