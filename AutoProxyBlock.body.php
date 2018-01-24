7160e21169f402442038d04379d7910e8f815b15
mainframe98 <k.s.werf@hotmail.com>
2018-01-24 20:51:34 +0100

    SECURITY: Don't query API with format=php
    This requires unserialization of potentially unsafe php code,
    which is dangerous, even if the source is trusted.
    Because the default configuration does not use https when
    querying the API, this causes a security vulnerability.
    Additionally, there is no urlencoding when building the
    request url for the API. This has been fixed as well.
    
    Bug: T185652
    Change-Id: I343ea8dba6d4137b95a69c0a2ea081aba7b936ba
    (cherry picked from commit bc3c9dab8896153677e87ff5ab8907ec5087d527)

--- AutoProxyBlock.body.php
+++ AutoProxyBlock.body.php
@@ -1,15 +1,15 @@
 <?php
- 
+
 class AutoProxyBlock {
 	static function isProxy( $ip ) {
 		global $wgMemc, $wgAutoProxyBlockSources;
-		
+
 		$memcKey = wfMemcKey( 'isproxy', $ip );
 		$data = $wgMemc->get( $memcKey );
-		
+
 		if( $data != '' ) {
 			return ( $data === 'proxy' ) ? true : false;
-		} else {		
+		} else {
 			if( isset($wgAutoProxyBlockSources['api']) ) {
 				foreach($wgAutoProxyBlockSources['api'] as $url) {
 					$request_options = array(
@@ -17,7 +17,7 @@ class AutoProxyBlock {
 						'list' => 'blocks',
 						'bkip' => $ip,
 						'bklimit' => '1',
-						'bkprop' => 'expiry|reason',					
+						'bkprop' => 'expiry|reason',
 					);
 					$ban = self::requestForeignAPI($url, $request_options);
 					if( isset($ban['query']['blocks'][0]) && preg_match($wgAutoProxyBlockSources['key'], $ban['query']['blocks'][0]['reason']) ) {
@@ -26,7 +26,7 @@ class AutoProxyBlock {
 					}
 				}
 			}
-			
+
 			if( isset($wgAutoProxyBlockSources['raw']) ) {
 				$list = array();
 				foreach($wgAutoProxyBlockSources['raw'] as $file) {
@@ -37,7 +37,7 @@ class AutoProxyBlock {
 						}
 					}
 				}
- 
+
 				if ( in_array( $ip, array_unique($list) ) ) {
 					$wgMemc->set( $memcKey, 'proxy', 60 * 60 * 24 );
 					return true;
@@ -46,24 +46,24 @@ class AutoProxyBlock {
 					return false;
 				}
 			}
-		
+
 			return false;
 		}
 	}
- 
+
 	static function checkProxy( $title, $user, $action, &$result ) {
 		global $wgProxyCanPerform, $wgAutoProxyBlockLog, $wgRequest;
-		
+
 		if( in_array( $action, $wgProxyCanPerform ) || $user->isAllowed('proxyunbannable') ) {
 			return true;
 		}
-		
+
 		$userIP = $wgRequest->getIP();
 		if( self::isProxy( $userIP ) ) {
 			if( $wgAutoProxyBlockLog ) {
 				$log = new LogPage( 'proxyblock' );
 				$log->addEntry( 'blocked', $title, false, array( $action, $user->getName() ) );
-				
+
 				// hack for 1.19-
 				$dbw = wfGetDB( DB_MASTER );
 				$blocker = User::newFromName( 'AutoProxyBlock' );
@@ -73,56 +73,53 @@ class AutoProxyBlock {
 					array( 'log_type' => 'proxyblock', 'log_user_text' => $user->getName() ),
 					__METHOD__,
 					array( 'ORDER BY' => 'log_timestamp DESC' )
-				);				
+				);
 			}
 			$result[] = array( 'proxy-blocked', $userIP );
-			return false;		   
+			return false;
 		}
-		
-		return true;		
+
+		return true;
 	}
- 
+
 	function AFSetVar( &$vars, $title ) {
 		global $wgRequest;
 		$vars->setVar( 'is_proxy', self::isProxy( $wgRequest->getIP() ) ? 1 : 0 );
 		return true;
 	}
- 
+
 	function AFBuilderVars( &$builder ) {
 		$builder['vars']['is_proxy'] = 'is-proxy';
 		return true;
 	}
- 
+
 	function tagProxyChange( $recentChange ) {
 		global $wgTagProxyActions, $wgUser, $wgRequest;
-		
+
 		if ( $wgTagProxyActions && self::isProxy( $wgRequest->getIP() ) && !$wgUser->isAllowed( 'notagproxychanges' ) ) {
-			ChangeTags::addTags( 
+			ChangeTags::addTags(
 				'proxy',
 				$recentChange->mAttribs['rc_id'],
 				$recentChange->mAttribs['rc_this_oldid'],
-				$recentChange->mAttribs['rc_logid'] 
+				$recentChange->mAttribs['rc_logid']
 			);
 		}
 		return true;
 	}
- 
+
 	static function addProxyTag( &$emptyTags ) {
 		global $wgTagProxyActions;
-		
+
 		if ( $wgTagProxyActions ) {
 			$emptyTags[] = 'proxy';
 		}
 		return true;
 	}
- 
+
 	static function requestForeignAPI( $url, $options ) {
-		$url .= '?format=php';
-		foreach($options as $param => $value) {
-			$url .= '&'.$param.'='.$value;
-		}
-		
-		$content = Http::get($url);
-		return unserialize($content);
+		$url .= '?format=json&' . wfArrayToCgi( $options );
+
+		$content = Http::get( $url );
+		return json_decode( $content, true );
 	}
 }
